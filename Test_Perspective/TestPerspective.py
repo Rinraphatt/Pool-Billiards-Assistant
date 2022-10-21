7c4dfb2211109172cadb216154190e30cf52f431
Pubodin Tieanthong <oatxshot555@gmail.com>
2022-10-21 21:29:00 +0700

    Sum

--- Test_Perspective/TestPerspective.py
+++ Test_Perspective/TestPerspective.py
@@ -13,18 +13,22 @@ while succuess:
     bl = (180 ,922)
     tr = (1741 ,155)
     br = (1742 ,925)
-    cv2.circle(frame, tl, 2, (0, 0, 255), -1)
-    cv2.circle(frame, bl, 2, (0, 0, 255), -1)
-    cv2.circle(frame, tr, 2, (0, 0, 255), -1)
-    cv2.circle(frame, br, 2, (0, 0, 255), -1)
+    cv2.circle(frame, tl, 3, (0, 0, 255), -1)
+    cv2.circle(frame, bl, 3, (0, 0, 255), -1)
+    cv2.circle(frame, tr, 3, (0, 0, 255), -1)
+    cv2.circle(frame, br, 3, (0, 0, 255), -1)
+    cv2.line(frame, tl, bl, (0, 255, 0), 2)
+    cv2.line(frame, bl, br, (0, 255, 0), 2)
+    cv2.line(frame, br, tr, (0, 255, 0), 2)
+    cv2.line(frame, tl, tr, (0, 255, 0), 2)
     pts1 = np.float32([tl, bl, tr, br])
     pts2 = np.float32([[0, 0], [0, heigth], [width, 0], [width, heigth]])
     
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
     # Compute the perspective transform M
     tansformed_frame = cv2.warpPerspective(frame, matrix, (width, heigth))
-
-    cv2.imshow("Test", frame)
     cv2.imshow("Test_Perspectice", tansformed_frame)
+    cv2.imshow("Test", frame)
+    
     if cv2.waitKey(1) == 27:
         break
