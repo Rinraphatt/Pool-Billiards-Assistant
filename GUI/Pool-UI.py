e39e240853dbb4fef26c0a51f897b5612f990d58
Rinraphat <iizaimmy@gmail.com>
2023-04-19 03:13:04 +0700

    edit resolution

--- GUI/Pool-UI.py
+++ GUI/Pool-UI.py
@@ -12,7 +12,7 @@ class Sampleapp(tk.Tk):
     def __init__(self, *args, **kwargs):  
           
         tk.Tk.__init__(self, *args, **kwargs)
-        self.geometry("1920x1080")
+        self.geometry("1920x880")
         container = tk.Frame(self)  
         container.pack(side="top", fill="both", expand = True)  
         container.grid_rowconfigure(0, weight=1)  
@@ -42,9 +42,9 @@ class StartPage(tk.Frame):
         bg.place(x=0, y=0)
         button = Button(self,text="Start",bg="#C24E4E",fg="white", bd=0,activebackground="#C24E4E",font=self.Myfont(40),   
                             command=lambda: controller.show_frame(PageOne))  
-        button.place(x=810,y=880,width=300,height=100)
+        button.place(x=825,y=717,width=280,height=90)
         lbl1 = Label(self, text="Welcome", bg="White" ,fg="Black", font=self.Myfont(50), anchor=CENTER)
-        lbl1.pack(padx=140, pady=140)
+        lbl1.pack(padx=80, pady=80)
         lbl2 = Label(self, text="Pool Billiard Assistant", bg="White" ,fg="Black", font=self.Myfont(50), anchor=CENTER)
         lbl2.pack(padx=0, pady=0)
 
@@ -64,10 +64,10 @@ class PageOne(tk.Frame):
 
         buttontrain = Button(self,text="Training",bg="#FFFFFF",fg="Black", bd=0,activebackground="#FFFFFF",font=self.Myfont(90),   
                             command=lambda: controller.show_frame(TrainMode))  
-        buttontrain.pack(pady=140,padx=140)
+        buttontrain.pack(pady=130,padx=130)
         button8pool = Button(self,text="8-Pool",bg="Black",fg="#FFFFFF", bd=0,activebackground="Black",activeforeground="#FFFFFF",font=self.Myfont(90),   
                             command=lambda: controller.show_frame(Pool8Mode))  
-        button8pool.pack(pady=110,padx=110)
+        button8pool.pack(pady=10,padx=10)
 
         buttonback = Button(self,image=self.btnback,bg="#FFFFFF",bd=0,activebackground="#FFFFFF",
                             command=lambda: controller.show_frame(StartPage))
@@ -90,12 +90,12 @@ class TrainMode(tk.Frame):
         # button
         btn_basic = Button(self,text="Basic", fg="#FFFFFF",bg="Black",bd=0,activebackground="#FFFFFF",font=self.Myfont(40),
                                 command=lambda: controller.show_frame(ModeBasic))
-        btn_basic.place(x=168,y=400,width=475,height=387)
+        btn_basic.place(x=262,y=420,width=405,height=320)
         btn_Amature = Button(self,text="Amature", fg="#FFFFFF",bg="Black",bd=0,activebackground="#FFFFFF",font=self.Myfont(40),
                                 command=lambda: controller.show_frame(ModeAmature))
-        btn_Amature.place(x=729,y=400,width=475,height=387)
+        btn_Amature.place(x=764,y=420,width=405,height=320)
         btn_Custom = Button(self,text="Custom", fg="#FFFFFF",bg="Black",bd=0,activebackground="#FFFFFF",font=self.Myfont(40),)
-        btn_Custom.place(x=1290,y=400,width=475,height=387)
+        btn_Custom.place(x=1265,y=420,width=405,height=320)
 
         buttonback = Button(self,image=self.btnback,bg="Black",bd=0,activebackground="Black",
                             command=lambda: controller.show_frame(StartPage))
@@ -116,9 +116,9 @@ class Pool8Mode(tk.Frame):
 
         button2 = Button(self,text="Single Player",bg="#E22424",fg="#FFFFFF", bd=0,activebackground="#E22424",font=self.Myfont(50),
                             command=lambda: controller.show_frame(Play8Pool))  
-        button2.place(x=190,y=755,width=690,height=140)
+        button2.place(x=273,y=680,width=620,height=130)
         button3 = Button(self,text="Multi Player",bg="#E22424",fg="#FFFFFF", bd=0,activebackground="#E22424",font=self.Myfont(50))  
-        button3.place(x=1050,y=755,width=690,height=140)
+        button3.place(x=1040,y=680,width=620,height=130)
 
         buttonback = Button(self,image=self.btnback,bg="Black",bd=0,activebackground="Black",
                             command=lambda: controller.show_frame(StartPage))
@@ -158,24 +158,24 @@ class ModeBasic(tk.Frame):
 
         btnnum1 = Button(self,text=self.num[0],bg="black",fg="#FFFFFF", bd=0,activeforeground="#E22424",activebackground="Black",font=self.Myfont(110)
                         , command=lambda *args: self.display(self.num[0]))
-        btnnum1.place(x=140,y=460,width=475,height=360)
+        btnnum1.place(x=262,y=435,width=405,height=310)
         btnnum2 = Button(self,text=self.num[1],bg="black",fg="#FFFFFF", bd=0,activeforeground="#E22424",activebackground="Black",font=self.Myfont(110)
                         , command=lambda *args: self.display(self.num[1]))
-        btnnum2.place(x=720,y=460,width=475,height=360)
+        btnnum2.place(x=764,y=435,width=405,height=310)
         btnnum3 = Button(self,text=self.num[2],bg="black",fg="#FFFFFF", bd=0,activeforeground="#E22424",activebackground="Black",font=self.Myfont(110)
                         , command=lambda *args: self.display(self.num[2]))
-        btnnum3.place(x= 1310,y=460,width=475,height=360)
+        btnnum3.place(x=1265,y=435,width=405,height=310)
 
     def showbtn(self):
         btnnum1 = Button(self,text=self.num[0],bg="black",fg="#FFFFFF", bd=0,activeforeground="#E22424",activebackground="Black",font=self.Myfont(110)
                         , command=lambda *args: self.display(self.num[0]))
-        btnnum1.place(x=140,y=460,width=475,height=360)
+        btnnum1.place(x=262,y=435,width=405,height=310)
         btnnum2 = Button(self,text=self.num[1],bg="black",fg="#FFFFFF", bd=0,activeforeground="#E22424",activebackground="Black",font=self.Myfont(110)
                         , command=lambda *args: self.display(self.num[1]))
-        btnnum2.place(x=720,y=460,width=475,height=360)
+        btnnum2.place(x=764,y=435,width=405,height=310)
         btnnum3 = Button(self,text=self.num[2],bg="black",fg="#FFFFFF", bd=0,activeforeground="#E22424",activebackground="Black",font=self.Myfont(110)
                         , command=lambda *args: self.display(self.num[2]))
-        btnnum3.place(x= 1310,y=460,width=475,height=360)
+        btnnum3.place(x=1265,y=435,width=405,height=310)
  
     def Myfont(self, sizefont):
         self.myfont = Font(family="Londrina Solid", size=sizefont)
@@ -270,13 +270,13 @@ class ModeAmature(tk.Frame):
         
         btnnum1 = Button(self,text=self.num[0],bg="black",fg="#FFFFFF", bd=0,activeforeground="#E22424",activebackground="Black",font=self.Myfont(110)
                         , command=lambda *args: self.display(self.num[0]))
-        btnnum1.place(x=140,y=460,width=475,height=360)
+        btnnum1.place(x=262,y=435,width=405,height=310)
         btnnum2 = Button(self,text=self.num[1],bg="black",fg="#FFFFFF", bd=0,activeforeground="#E22424",activebackground="Black",font=self.Myfont(110)
                         , command=lambda *args: self.display(self.num[1]))
-        btnnum2.place(x=720,y=460,width=475,height=360)
+        btnnum2.place(x=764,y=435,width=405,height=310)
         btnnum3 = Button(self,text=self.num[2],bg="black",fg="#FFFFFF", bd=0,activeforeground="#E22424",activebackground="Black",font=self.Myfont(110)
                         , command=lambda *args: self.display(self.num[2]))
-        btnnum3.place(x= 1310,y=460,width=475,height=360)
+        btnnum3.place(x=1265,y=435,width=405,height=310)
 
 
     def Myfont(self, sizefont):
